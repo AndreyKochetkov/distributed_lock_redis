ab3b066d56740f88bb5431c3b801306bd0e559aa
Andrey Kochetkov <ciel_na@mail.ru>
2017-03-19 13:04:41 +0300

    modify app_start for Generator v.1.0

--- app_start.py
+++ app_start.py
@@ -1,5 +1,14 @@
+from time import sleep, clock
+from Generator import Generator
+
 def main():
-    pass
+    # TODO: сделать так, чтобы можно было определить через redis сколько приложений запущено
+    generator = Generator("localhost", 6379, 0, 1)
+    try:
+        generator.generate_messages()
+    except Exception as e:
+        print("Error: " + str(e))
+
 
 
 
