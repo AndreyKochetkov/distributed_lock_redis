a9eedd43bf6811fce3a13f60b2f86371eac768c9
Andrey Kochetkov <ciel_na@mail.ru>
2017-03-19 19:05:39 +0300

    modify generator v.1.1

--- Generator.py
+++ Generator.py
@@ -6,30 +6,29 @@ from faker import Factory
 class Generator:
     amount_of_messages = 10000000
     loop_for_updt_lock = 5
+    key_of_queue = "queue"
 
-    def __init__(self, host, port, db, id_of_gen):
-        self.host = host
-        self.port = port
-        self.db = db
+    def __init__(self, cursor, id_of_gen):
+        self.cursor = cursor
         self.id_of_gen = id_of_gen
 
     # property?
     def generate_messages(self):
-        cursor = redis.StrictRedis(host=self.host, port=self.port, db=self.db)
 
-        self._set_lock(cursor, True)
+        self._set_lock(True)
 
         fake = Factory.create()
         for i in range(Generator.amount_of_messages):  # all messages of this generator
+            # TODO сделать это конвеером
             for j in range(Generator.loop_for_updt_lock):  # messages for update lock
                 sleep(0.5)
                 message = fake.text(max_nb_chars=100)
-                cursor.rpush("queue", message)
+                self.cursor.rpush(Generator.key_of_queue, message)
                 print(message)
 
-            self._set_lock(cursor, False)  # update lock
+            self._set_lock(False)  # update lock
 
-    def _set_lock(self, cursor, nx=True):
-        status_lock = cursor.set("lock", self.id_of_gen, ex=5, nx=nx)
+    def _set_lock(self, nx=True):
+        status_lock = self.cursor.set("lock", self.id_of_gen, ex=5, nx=nx)
         if status_lock is not True:
             raise Exception("Set lock is failed")
