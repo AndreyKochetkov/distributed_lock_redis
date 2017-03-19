a509d69bea16f7059d26133430ea3d1255a5d8c0
Andrey Kochetkov <ciel_na@mail.ru>
2017-03-19 19:07:13 +0300

    modify app_start for current versions of Generator v.1.1 and Handler v.1.0.

--- app_start.py
+++ app_start.py
@@ -1,13 +1,36 @@
 from time import sleep, clock
 from Generator import Generator
+from Handler import Handler
+import redis
 
 def main():
-    # TODO: сделать так, чтобы можно было определить через redis сколько приложений запущено
-    generator = Generator("localhost", 6379, 0, 1)
+    host = "localhost"
+    port = 6379
+    db = 0
+
+    cursor = redis.StrictRedis(host=host, port=port, db=db)
+
+    id_of_instance = cursor.incr("amount_of_instances")
+
+    generator = Generator(cursor, id_of_instance)
     try:
         generator.generate_messages()
     except Exception as e:
         print("Error: " + str(e))
+
+    handler = Handler(cursor, id_of_instance)
+
+    try:
+        while True:
+            handler.handle_messages(Generator.key_of_queue)
+            try:
+                generator.generate_messages()
+            except Exception as e:
+                print("Try of capture lock \n Error: " + str(e))
+
+    except Exception as e:
+        print("Error: " + str(e))
+    print("усе")
 
 
 
